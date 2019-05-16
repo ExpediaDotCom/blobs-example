81e1df7d82cb392fbd3533ce195dee80353d884a
vaibhavsawhney <vaibhavsawhney1511@gmail.com>
2019-05-16 12:34:14 +0530

    Benchmarking
    Added the code to define the benchmarking of the application through JMH, a powerfull tool for benchmarking. Currently we are finding out the throughput and average respose time of the application with and without the blobs code to show that there is no impact on the application with blobs library attached to it

--- src/main/java/com/blobExample/SampleApplication.java
+++ src/main/java/com/blobExample/SampleApplication.java
@@ -2,6 +2,15 @@ package com.blobExample;
 
 import com.blobExample.client.ClientApplication;
 import com.blobExample.server.ServerApplication;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 public class SampleApplication {
     public static void main(String[] args) throws Exception {
@@ -15,8 +24,61 @@ public class SampleApplication {
         else if(String.format("sampleServer").equalsIgnoreCase(args[0])){
             new ServerApplication().start(args);
         }
+        else if(String.format("benchmark").equalsIgnoreCase(args[0])){
+            initBenchmarking();
+        }
         else{
             throw new Exception(String.format("unexpected argument").concat(" ").concat(args[0]));
         }
+
     }
+
+    private static void initBenchmarking() {
+        ResultFormatType resultsFileOutputType = ResultFormatType.JSON;
+        String resultFilePrefix = "jmh-";
+
+        Options opt = new OptionsBuilder()
+                .include(".*Benchmark")
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .forks(1)
+                .threads(1)
+                .resultFormat(resultsFileOutputType)
+                .result(buildResultsFileName(resultFilePrefix, resultsFileOutputType))
+                .jvmArgs("-server", "-Xms2048m", "-Xmx2048m")
+                .build();
+        try {
+            new Runner(opt).run();
+        } catch (RunnerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String buildResultsFileName(String resultFilePrefix, ResultFormatType resultType) {
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+
+        String suffix;
+        switch (resultType) {
+            case CSV:
+                suffix = ".csv";
+                break;
+            case SCSV:
+                // Semi-colon separated values
+                suffix = ".scsv";
+                break;
+            case LATEX:
+                suffix = ".tex";
+                break;
+            case JSON:
+            default:
+                suffix = ".json";
+                break;
+
+        }
+
+        return String.format("%s%s%s", resultFilePrefix, date.format(formatter), suffix);
+    }
+
 }
---
+++ src/main/java/com/blobExample/benchmarking/BenchmarkServiceForBlobAttach.java
@@ -0,0 +1,110 @@
+package com.blobExample.benchmarking;
+
+import com.blobExample.client.ClientResource;
+import com.blobExample.models.ClientRequest;
+import com.blobExample.models.ClientResponse;
+import com.blobExample.models.ServerResponse;
+import com.expedia.blobs.core.io.FileStore;
+import java.io.File;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.mockito.Mockito;
+import org.openjdk.jmh.annotations.*;
+
+
+import static org.mockito.Mockito.when;
+
+public class BenchmarkServiceForBlobAttach {
+
+    @State(Scope.Benchmark)
+    public static class ClientState {
+
+        @Setup(Level.Iteration)
+        public void doSetup() {
+            System.out.println("doSetup Called");
+
+            template = "Hello ServerResource. I am %s!";
+            defaultName = "BlobsBenchmark";
+            client = Mockito.mock(Client.class);
+
+            blobStore = setupBlobStore();
+
+            clientRequest = new ClientRequest(defaultName, String.format(template, defaultName));
+
+            serverResponse = new ServerResponse("BlobServer", "");
+        }
+
+        @TearDown(Level.Iteration)
+        public void tearDown() {
+            System.out.println("Tear down called");
+            if (blobStore != null)
+                blobStore.close();
+        }
+
+        private FileStore setupBlobStore() {
+            File directory = setupBlobsDirectory();
+
+            return new FileStore(directory, 8 , 180);
+        }
+
+        private File setupBlobsDirectory() {
+            String userDirectory = System.getProperty("user.dir");
+            String directoryPath = new String(userDirectory).concat("/BenchmarkingBlobs");
+            File directory = new File(directoryPath);
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+
+            return directory;
+        }
+
+        public ClientRequest clientRequest;
+        public ServerResponse serverResponse;
+        private FileStore blobStore;
+        private String template;
+        private String defaultName;
+        private Client client;
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    public ClientResponse blobsEnabled(ClientState state) {
+
+        ClientResource clientResource = new ClientResource(state.template, state.defaultName, state.client, state.blobStore);
+
+        setupBehaviour(state, clientResource);
+
+        return clientResource.getMessageFromServer(null);
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    public ClientResponse blobsDisabled(ClientState state) {
+
+        ClientResource clientResource = new ClientResource(state.template, state.defaultName, state.client, null);
+
+        setupBehaviour(state, clientResource);
+
+        return clientResource.getMessageFromServer(null);
+    }
+
+    private void setupBehaviour(ClientState state, ClientResource clientResource) {
+        Response mockedResponse = Mockito.mock(Response.class);
+        Invocation.Builder invocationBuilder = Mockito.mock(Invocation.Builder.class);
+        WebTarget webTarget = Mockito.mock(WebTarget.class);
+
+        when(clientResource.getJerseyClient().target("http://localhost:9090/hi")).thenReturn(webTarget);
+
+        when(webTarget.request(MediaType.APPLICATION_JSON_TYPE)).thenReturn(invocationBuilder);
+
+        when(invocationBuilder.post(
+                Mockito.any(Entity.class)
+        )).thenReturn(mockedResponse);
+
+        when(mockedResponse.readEntity(ServerResponse.class)).thenReturn(state.serverResponse);
+    }
+}
